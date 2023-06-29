f3726483baae8c7d9af5466c480298df72e42b94
mateeusduarte <mateus2050@gmail.com>
2023-06-29 08:57:22 -0300

    feat: header

---
+++ lighthouse-front/src/components/Header/index.tsx
@@ -0,0 +1,24 @@
+import HeaderLink from "../HeaderLink";
+
+export default function Header() {
+  return (
+    <header className="bg-primary h-xxxl border-b-8 border-secondary flex items-center justify-between py-lg px-lg w-full">
+      <h1
+        className="text-secondary"
+        style={{ font: "normal normal normal 30px Aboreto" }}
+      >
+        LIGHTHOUSE
+      </h1>
+      <div className="flex items-center gap-xxxs">
+        <HeaderLink isActive>Home</HeaderLink>
+        <HeaderLink>Quem Somos</HeaderLink>
+        <HeaderLink>Soluções</HeaderLink>
+        <HeaderLink>Metodologia</HeaderLink>
+        <HeaderLink>Blog</HeaderLink>
+      </div>
+      <button className="text-black bg-secondary p-4 px-sm rounded font-bold hover:bg-secondaryDarker ">
+        Contato
+      </button>
+    </header>
+  );
+}
