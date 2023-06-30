07b3ef9235d03242eef780361f4edb308e997769
mateeusduarte <mateus2050@gmail.com>
2023-06-30 16:35:39 -0300

    f

--- lighthouse-front/src/components/ExperienceSession/index.tsx
+++ lighthouse-front/src/components/ExperienceSession/index.tsx
@@ -1,11 +1,63 @@
-import { ExperienceContainer, Filter, ExperienceTitle } from "./styles";
+import {
+  ExperienceContainer,
+  Filter,
+  ExperienceTitle,
+  ExperienceDescription,
+  GlowingCardsContainer,
+  GlowingCard,
+  Divider,
+  ExperienceDescriptionCard,
+  CardTitle,
+  CardDescription,
+} from "./styles";
 
 export default function ExperienceSession() {
   return (
     <ExperienceContainer>
       <Filter />
+      <div style={{ paddingTop: 200, zIndex: 999 }}>
+        <ExperienceTitle>EXPERIÊNCIA E MÉTODO</ExperienceTitle>
 
-      <ExperienceTitle>EXPERIÊNCIA E MÉTODO</ExperienceTitle>
+        <ExperienceDescription>
+          PARA NAVEGAR EM DIFERENTES CENARIOS!
+        </ExperienceDescription>
+      </div>
+      <Divider />
+      <GlowingCardsContainer>
+        <GlowingCard>
+          <CardTitle>CONSULTORIA OPERACIONAL</CardTitle>
+          <ExperienceDescriptionCard>
+            <CardDescription>Corretora</CardDescription>
+          </ExperienceDescriptionCard>
+          <ExperienceDescriptionCard>
+            <CardDescription>
+              Software de <br />
+              gestão de
+              <br /> investimentos
+            </CardDescription>
+          </ExperienceDescriptionCard>
+        </GlowingCard>
+
+        <GlowingCard>
+          <CardTitle>CONSULTORIA JURÍDICA</CardTitle>
+          <CardDescription>
+            Aspectos legais sobre <br /> ativos de investidores
+            <br /> brasileiros em
+            <br />
+            jurisdição internacional
+          </CardDescription>
+        </GlowingCard>
+
+        <GlowingCard>
+          <CardTitle>CONSULTORIA TRIBUTÁRIA</CardTitle>
+        </GlowingCard>
+        <GlowingCard>
+          <CardTitle>CONSULTORIA FINANCEIRA</CardTitle>
+        </GlowingCard>
+        <GlowingCard>
+          <CardTitle>TRADING STRATEGIES</CardTitle>
+        </GlowingCard>
+      </GlowingCardsContainer>
     </ExperienceContainer>
   );
 }
--- lighthouse-front/src/components/ExperienceSession/styles.ts
+++ lighthouse-front/src/components/ExperienceSession/styles.ts
@@ -33,15 +33,70 @@ export const ExperienceTitle = styled.h1`
   z-index: 999;
 `;
 
+export const CardTitle = styled.h1`
+  text-align: center;
+  font: normal normal normal 20px/22px Aboreto;
+  letter-spacing: 1px;
+  color: #dbc68f;
+  margin: 0;
+  z-index: 999;
+`;
+
+export const CardDescription = styled.p`
+  text-align: center;
+  font: normal normal 300 20px/21px Times New Roman;
+  letter-spacing: 0px;
+  color: #fafafa;
+  opacity: 1;
+  z-index: 999;
+  margin: 0;
+`;
 export const ExperienceDescription = styled.p`
   text-align: center;
-  font: normal normal 500 20px/31px Open Sans;
+  font: normal normal 500 30px/41px Open Sans;
   @media (max-width: 884px) {
-    font: normal normal 500 15px/21px Open Sans;
+    font: normal normal 500 25px/31px Open Sans;
   }
   letter-spacing: 0px;
   color: #fafafa;
   opacity: 1;
   z-index: 999;
   margin: 0;
+`;
+
+export const Divider = styled.div`
+  background: #dbc68f;
+  width: 40%;
+  height: 3px;
+  margin-top: 8px;
+  transform: matrix(-1, 0, 0, -1, 0, 0);
+  z-index: 999;
+`;
+
+export const GlowingCardsContainer = styled.div`
+  display: flex;
+  z-index: 999;
+  margin-top: 70px;
+  align-items: center;
+  width: 90%;
+  gap: 20px;
+`;
+
+export const GlowingCard = styled.div`
+  min-height: 250px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  box-shadow: 0px 5px 10px #ffffff;
+  border: 5px solid #e4c78d;
+  border-radius: 20px;
+  opacity: 1;
+  z-index: 999;
+  gap: 40px;
+  padding: 50px 20px;
+`;
+
+export const ExperienceDescriptionCard = styled.div`
+  border-bottom: 2px solid #dbc68f;
 `;
