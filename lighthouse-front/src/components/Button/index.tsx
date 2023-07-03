9deb5ebfa1575c816e92aa50ed0f2b868e1f8d9a
mateeusduarte <mateus2050@gmail.com>
2023-07-03 09:11:36 -0300

    a

--- lighthouse-front/src/components/Button/index.tsx
+++ lighthouse-front/src/components/Button/index.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { ButtonContainer } from "./styles";
 
-export type ButtonType = "primary" | "secondary" | "tertiary";
+export type ButtonType = "primary" | "secondary" | "tertiary" | "round";
 
 interface ButtonProps {
   type: ButtonType;
   children: React.ReactNode;
   height?: number | string;
   width?: number | string;
+  onClick?: () => void;
   minWidth?: number | string;
 }
 
@@ -16,6 +17,7 @@ const Button: React.FC<ButtonProps> = ({
   children,
   height,
   width,
+  onClick,
   minWidth,
 }) => {
   const colors = {
@@ -26,28 +28,33 @@ const Button: React.FC<ButtonProps> = ({
     primary: "black",
     secondary: "white",
     tertiary: "white",
+    round: "black",
   };
   const buttonBackgroundColor = {
     primary: colors.secondary,
     secondary: `linear-gradient(to right, ${colors.secondary}, ${colors.primary})`,
     tertiary: "transparent",
+    round: colors.secondary,
   };
 
   const border = {
     primary: "none",
     secondary: `2px solid ${colors.secondary}`,
     tertiary: `2px solid ${colors.secondary}`,
+    round: "none",
   };
 
   const borderRadius = {
     primary: 4,
     secondary: 4,
     tertiary: 12,
+    round: "50%",
   };
 
   const font = {
     primary: "normal normal bold 20px/27px Open Sans",
     secondary: "normal normal bold 18px Marine",
+    tertiary: "normal normal normal 20px/37px Times New Roman",
     tertiary: "normal normal normal 20px/37px Times New Roman",
   };
 
@@ -55,9 +62,11 @@ const Button: React.FC<ButtonProps> = ({
     primary: "0",
     secondary: "0px",
     tertiary: "0 32px",
+    round: "0px",
   };
   return (
     <ButtonContainer
+      onClick={onClick}
       style={{
         color: buttonColor[type],
         background: buttonBackgroundColor[type],
